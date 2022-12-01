7b7e1aeebc9fd5b2c0dd548a0017a3dc0d7425c4
RubsRafa <rubiarafaela2000@gmail.com>
2022-12-01 20:58:26 -0300

    requisito: flashcard completo

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
 import Logo from './zaprecall/assets/img/logo.png'
 
@@ -7,7 +7,18 @@ import Cards from './componentes/Cards'
 
 export default function App() {
 
-  
+  const cards = [
+    { question: "O que é JSX?", answer: "Uma extensão da linguagem JavaScript" },
+    { question: "O React é __", answer: "Uma biblioteca JavaScript para construção de interfaces" },
+    { question: "Componentes devem iniciar com __", answer: "Letra maiúscula" },
+    { question: "Podemos colocar __ dentro do JSX", answer: "expressões" },
+    { question: "O ReactDOM nos ajuda __", answer: "Interagindo com a DOM para colocar componentes React na mesma" },
+    { question: "Usamos o npm para __", answer: "Gerenciar os pacotes necessários e suas dependências" },
+    { question: "Usamos props para __", answer: "Passar diferentes informações para componentes" },
+    { question: "Usamos estado (state) para __", answer: "Dizer para o React quais informações quando atualizadas devem renderizar a tela novamente" }
+]
+
+const [respondidas, setRespondidas] = useState(0); 
 
   return (
     <Container>
@@ -16,10 +27,10 @@ export default function App() {
         <h1>ZapRecall</h1>
       </LogoContainer>
 
-      <Cards />
-      
+      <Cards cards={cards} setRespondidas={setRespondidas} respondidas={respondidas} />
+
       <Footer>
-        0/8 CONCLUÍDOS
+        {respondidas}/{cards.length} CONCLUÍDOS
       </Footer>
     </Container>
   );
--- src/componentes/Cards.js
+++ src/componentes/Cards.js
@@ -7,48 +7,102 @@ import Certo from '../zaprecall/assets/img/icone_certo.png'
 import Quase from '../zaprecall/assets/img/icone_quase.png'
 import Errado from '../zaprecall/assets/img/icone_erro.png'
 
-export default function Cards() {
+export default function Cards({ cards, setRespondidas, respondidas }) {
 
-    const cards = [
-        { question: "O que é JSX?", answer: "Uma extensão da linguagem JavaScript" },
-        { question: "O React é __", answer: "Uma biblioteca JavaScript para construção de interfaces" },
-        { question: "Componentes devem iniciar com __", answer: "Letra maiúscula" },
-        { question: "Podemos colocar __ dentro do JSX", answer: "expressões" },
-        { question: "O ReactDOM nos ajuda __", answer: "Interagindo com a DOM para colocar componentes React na mesma" },
-        { question: "Usamos o npm para __", answer: "Gerenciar os pacotes necessários e suas dependências" },
-        { question: "Usamos props para __", answer: "Passar diferentes informações para componentes" },
-        { question: "Usamos estado (state) para __", answer: "Dizer para o React quais informações quando atualizadas devem renderizar a tela novamente" }
-    ]
-    const [cardClicado, setCardClicado] = useState(false)
+  const [pergunta, setPergunta] = useState(false)       //se a pergunta vai aparecer
+  const [resposta, setResposta] = useState(false)       //se a resposta vai aparecer
+  const [numeroCards, setNumeroCards] = useState();     //numero da pergunta selecionada
+  const [arrayCards, setArrayCards] = useState([])      //todas as perguntas já selecionadas
+  const [resultado, setResultado] = useState (false)    //se o resultao vai aparecer
+  const [cor, setCor] = useState ('')                   //define botao resultado da resposta
+  const [cardsRespondidas, setCardsRespondidas] = useState([])
 
-    return (
-        <>
-        
-            {cards.map((c, i) => <>
-                <CaixaPerguntaFechada onClick={() => console.log (c.question, i + 1)}>
-                    <PerguntasTexto>Pergunta {i + 1}</PerguntasTexto>
-                    <img alt='' src={SetaPlay} />
-                </CaixaPerguntaFechada>
+  const [errados, setErrados] = useState([]);
+  const [quases, setQuases] = useState([]);
+  const [certos, setCertos] = useState([]);
 
-                <CaixaPerguntaAberta>
-                    <h1>{c.question}</h1>
-                    <PerguntasImagem onClick={() => console.log (c.answer)} src={SetaVirar} />
-                </CaixaPerguntaAberta>
+  function addNumerosCards(i) {
+    console.log ('addNumerosCards')
+    if (!arrayCards.includes (i)) {
+      setArrayCards([...arrayCards, i])
+    }
+    console.log(i)
+    setNumeroCards(i);
+    setPergunta(true)
+  }
 
-                <CaixaPerguntaAberta>
-                    <h1>{c.answer}</h1>
-                    <Botoes>
-                        <BotaoVermelho onClick={() => console.log ('pergunta ' + (i + 1) + ' vermelho')}>Não lembrei</BotaoVermelho>
-                        <BotaoAmarelo onClick={() => console.log ('pergunta ' + (i + 1) + ' amarelo')}>Quase não lembrei</BotaoAmarelo>
-                        <BotaoVerde onClick={() => console.log ('pergunta ' + (i + 1) + ' verde')}>Zap!</BotaoVerde>
-                        <BotaoCinza>OI</BotaoCinza>
-                    </Botoes>
-                </CaixaPerguntaAberta>
-            </>
-            )}
+  function mostrarResposta() {
+    console.log ('mostrarResposta')
+    setPergunta (false)
+    setResposta(true)
+  }
+
+  function esconderTudo(cor, i) {
+    console.log ('esconderTu')
+    setResultado (true)
+    setPergunta(false)
+    setResposta(false)
+    setCor (cor)
+    setCardsRespondidas ([...cardsRespondidas, i])
+
+    if (cor == Errado) {
+      setErrados ([...errados, i])
+    }
+    if (cor == Quase) {
+      setQuases ([...quases, i])
+    }
+    if (cor == Certo) {
+      setCertos ([...certos, i])
+    } 
+
+    setRespondidas(respondidas + 1)
+
+  }
+  return (
+    <>
+
+      {cards.map((c, i) => <>
+        {numeroCards !== (i + 1) && !arrayCards.includes(i+1) && (<CaixaPerguntaFechada onClick={() => addNumerosCards(i + 1)}>
+          <PerguntasTexto>Pergunta {i + 1}</PerguntasTexto>
+          <img alt='' src={SetaPlay} />
+        </CaixaPerguntaFechada>)}
+
+        {pergunta && numeroCards == (i + 1) && (
+          <CaixaPerguntaAberta>
+            <h1>{c.question}</h1>
+            <PerguntasImagem onClick={() => mostrarResposta()} src={SetaVirar} />
+          </CaixaPerguntaAberta>
+        )}
+
+        {resposta && numeroCards == (i + 1) && (
+          <CaixaPerguntaAberta>
+            <h1>{c.answer}</h1>
+            <Botoes>
+              <BotaoVermelho onClick={() => esconderTudo(Errado, i)} >Não lembrei</BotaoVermelho>
+              <BotaoAmarelo onClick={() => esconderTudo(Quase, i)} >Quase não lembrei</BotaoAmarelo>
+              <BotaoVerde onClick={() => esconderTudo(Certo, i)}>Zap!</BotaoVerde>
+              <BotaoCinza>OI</BotaoCinza>
+            </Botoes>
+          </CaixaPerguntaAberta>
+        )}
+
+        {resultado && cardsRespondidas.includes(i) && (
+          <CaixaPerguntaFechada>
+          <PerguntasTexto>Pergunta {i + 1}</PerguntasTexto>
+          <img alt='' 
+          src={errados.includes(i) && Errado || quases.includes(i) && Quase || certos.includes(i) && Certo}/>
+        </CaixaPerguntaFechada>
+        )}
+
+        {/* <CaixaPerguntaFechada onClick={() => console.log(c.question)}>
+          <PerguntasTexto>Pergunta {i + 1}</PerguntasTexto>
+          <img alt='' src={Certo} />
+        </CaixaPerguntaFechada> */}
+      </>
+      )}
 
 
-            <CaixaPerguntaFechada>
+      {/* <CaixaPerguntaFechada>
                 <PerguntasTexto>Pergunta 1</PerguntasTexto>
                 <img alt='' src={Certo} />
             </CaixaPerguntaFechada>
@@ -71,9 +125,9 @@ export default function Cards() {
                     <BotaoVerde>Zap!</BotaoVerde>
                     <BotaoCinza>OI</BotaoCinza>
                 </Botoes>
-            </CaixaPerguntaAberta>
-        </>
-    )
+            </CaixaPerguntaAberta> */}
+    </>
+  )
 }
 
 const Botoes = styled.div`
@@ -101,6 +155,7 @@ font-family: 'Recursive';
   font-size: 16px;
   line-height: 19px;
   color: #333333;
+  color: ${props => props.cor}
 `;
 const PerguntasImagem = styled.img`
 position: absolute;
